94b1f30d1496eef4493d1ab52fd8be944eaae1c7
Hristian2314 <f75@abv.bg>
2024-02-21 09:43:08 +0200

    defgdfg

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ListEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> listEven = new List<int>();
+            List<int> listOdd = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 == 0)
+                {
+                    listEven.Add(list[i]);
+                }
+                else
+                {
+                    listOdd.Add(list[i]);
+                }
+                Console.WriteLine($"Chetni " + string.Join(" ", listEven));
+                Console.WriteLine($"Negative" +  string.Join(" ", listOdd));
+            }
+
+        }
+        static void Geteven(List<int> imput)
+        {
+
+        }
+    }
+}
